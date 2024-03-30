8fcb84b00db06d9f3295a42d08042a2f0ef90fbd
joyceravie <joyce.rvs@hotmail.com>
2024-03-30 19:57:50 -0300

    adiciona menu

--- Menu.ts
+++ Menu.ts
@@ -1,17 +1,9 @@
 import readlinesync = require('readline-sync');
 import { colors } from './src/util/Cores';
-import { Conta } from './src/model/Conta';
+
 
 export  function main() {
   let opcao: number;
-
-  // Objeto da Classe Conta (Teste)
-  const conta: Conta = new Conta(1, 123, 1, "Joyce", 10000);
-  conta.visualizar();
-  conta.sacar(10500);
-  conta.visualizar();
-  conta.depositar(5000);
-  conta.visualizar();
   
   while(true){
 
